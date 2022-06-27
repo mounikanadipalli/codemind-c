3b69973a1acdf699e61a41ef175915d055c564a8
mounikanadipalli <97655206+mounikanadipalli@users.noreply.github.com>
2022-06-27 23:11:43 +0530

    #include<stdio.h> int main() {     int n,i,se,a[100],d=0;     float c=0,sum=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         sum+=a[i];     }     c=float(sum/n);     printf("%.2f",c); }

---
+++ average_of_the_array.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,se,a[100],d=0;
+    float c=0,sum=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        sum+=a[i];
+    }
+    c=float(sum/n);
+    printf("%.2f",c);
+}
